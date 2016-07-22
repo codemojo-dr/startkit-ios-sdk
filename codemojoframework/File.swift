fe1b40e0808196e18577bcfd316d44532bf4365f
Mayilkannan <mayilkannan@abservetech.com>
2016-07-22 12:26:11 +0530

    Implement Accesstoken and getwallet service -- mayilkannan

--- codemojoframework/File.swift
+++ codemojoframework/File.swift
@@ -10,31 +10,157 @@ import Foundation
 
 public class mojoconfig {
 
+    var Sandbox_url = "https://sandbox.codemojo.io/"
+    
+    var Production_url = "https://production.codemojo.io/"
+    
+    var Access_token = ""
+    
+    var Environment:Int = 0
+    
     public init() {
         
     }
     
-    public func createReferralCode(username: String, length: Int) -> String {
+    public func getAppSecret(app_token:String, customer_id:String, EnviromentType:Int, completion: (status: String, access_token: String) -> Void) {
         
-        let allowedChars = "\(username)abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        let allowedCharsCount = UInt32(allowedChars.characters.count)
-        var randomString = ""
+        var urlString = ""
         
-        for _ in (0..<length) {
-            let randomNum = Int(arc4random_uniform(allowedCharsCount))
-            let newCharacter = allowedChars[allowedChars.startIndex.advancedBy(randomNum)]
-            randomString += String(newCharacter)
+        self.Environment = EnviromentType
+        
+        if EnviromentType == 0 {
+           urlString = Sandbox_url
         }
         
-        return randomString
+        else {
+           urlString = Production_url
+        }
+        
+        let url:NSURL = NSURL(string: urlString+"oauth/app")!
+        let session = NSURLSession.sharedSession()
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
+        
+        let paramString = "app_token=\(app_token)&customer_id=\(customer_id)"
+        request.HTTPBody = paramString.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let task = session.dataTaskWithRequest(request) {
+            (
+            let data, let response, let error) in
+            
+            guard let _:NSData = data, let _:NSURLResponse = response  where error == nil else {
+                
+                completion(status: "Fail", access_token: "")
+                
+                return
+            }
+            
+            do {
+                if let responseObject = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? [String: AnyObject] {
+                    
+                    if responseObject["access_token"] != nil {
+                        
+                        let access_token = responseObject["access_token"] as! String
+                        
+                        print("access token \(access_token)")
+                        
+                        self.Access_token = access_token
+                        
+                        completion(status: "Success", access_token: access_token)
+                        
+                    }
+                    
+                }
+            } catch {
+                print(error)
+                print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+                
+                completion(status: "Fail", access_token: "")
+                
+            }
+            
+        }
+        
+        task.resume()
+        
     }
     
-    public func createloyalitypoint(value: Double, percentage: Double) -> Int {
+    public func getBalance(customer_id:String, completion: (status: String, total: Int) -> Void) {
         
-        let percentval = ((value/100)*percentage) as Double
+        var urlString = ""
         
-        let result = Int(percentval)
+        if Environment == 0 {
+            urlString = Sandbox_url
+        }
+            
+        else {
+            urlString = Production_url
+        }
         
-        return result
+        let url:NSURL = NSURL(string: urlString+"v1/services/wallet/credits/balance/user=\(customer_id)")!
+        
+        print("url1 \(url)")
+        
+        let session1 = NSURLSession.sharedSession()
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "GET"
+        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
+        
+        request.setValue("Bearer "+Access_token, forHTTPHeaderField: "Authorization")
+        
+        let task = session1.dataTaskWithRequest(request) {
+            (
+            let data, let response, let error) in
+            
+            guard let _:NSData = data, let _:NSURLResponse = response  where error == nil else {
+                print("error")
+                
+                completion(status: "Fail", total: 0)
+                
+                return
+            }
+            
+            do {
+                if let responseObject = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? [String: AnyObject] {
+                    print("response \(responseObject)")
+                    
+                    if responseObject["results"] != nil {
+                        
+                        let results = responseObject["results"] as! NSDictionary
+                        
+                        if results.objectForKey("total") != nil {
+                            
+                            let total = results.objectForKey("total") as! NSNumber
+                            
+                            print("total \(total)")
+                            
+                            completion(status: "Success", total: total as Int)
+                            
+                        }
+                        else {
+                            completion(status: "Fail", total: 0)
+                        }
+                        
+                    }
+                    else {
+                        completion(status: "Fail", total: 0)
+                    }
+                    
+                }
+            } catch {
+                print(error)
+                print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+                
+                completion(status: "Fail", total: 0)
+                
+            }
+            
+        }
+        
+        task.resume()
     }
+   
 }
